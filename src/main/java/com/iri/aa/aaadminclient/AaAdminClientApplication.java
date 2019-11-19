847c5e081ac9bf996de590a9be10aaa1bb81fea1
Konstantinos Papadopoulos <Konstantinos.Papadopoulos@IRIworldwide.com>
2019-11-19 16:36:22 +0200

    Initial approach of a Spring Boot Admin Client with HTTP basic authentication.

---
+++ src/main/java/com/iri/aa/aaadminclient/AaAdminClientApplication.java
@@ -0,0 +1,13 @@
+package com.iri.aa.aaadminclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AaAdminClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AaAdminClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iri/aa/aaadminclient/AaAdminClientApplicationTests.java
@@ -0,0 +1,16 @@
+package com.iri.aa.aaadminclient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AaAdminClientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
